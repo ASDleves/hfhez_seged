9cdf1cbf699347e2cd4683b0f7a0baf203367735
ASDleves <ricsi20020302@gmail.com>
2023-11-14 14:23:11 +0100

    tablazat megjelen, torles gombbal, +szerkesztes, gomb nelkül

--- frontend/mvc/controller/Controller.js
+++ frontend/mvc/controller/Controller.js
@@ -1,11 +1,13 @@
 import DataService from "../model/DataService.js";
 import UrlapView from "../View/View.js"
 import UrlapModel from "../model/Urlapmodel.js"
-
+import Megjelenit from "../View/TablazatView/TablazatMegjelenit.js";
 class Controller{
     constructor(){
         this.urlapModel = new UrlapModel();
         this.urlapView = new UrlapView($(".urlap"), this.urlapModel.leiro);
+        this.dataService = new DataService();
+        this.dataService.getAxiosData("http://localhost:8000/api/writers", this.megjelenites, this.hibakezeles);
         this.submitElem = $("#submit")
         this.submitElem.on("click", (event) => {
             event.preventDefault()
@@ -14,8 +16,6 @@ class Controller{
             let isFormValid = true; 
             urlapelemLista.forEach((elem) => {
                 isFormValid = isFormValid && elem.getvalid();
-
-                
             });
             if (isFormValid) {
                 console.log("valid az űrlap!")
@@ -27,20 +27,35 @@ class Controller{
             } else {
                 console.log("Nem valid az űrlap!")
             }
-            this.dataService.postAxiosData("http://localhost:8000/writers", {
+            this.dataService.postAxiosData("http://localhost:8000/api/writers", {
                 "nev": urlapadat.nev,
                 "szul": urlapadat.szul
             });
             console.log(urlapadat)
+            
         })
-
-        this.dataService = new DataService();
-        this.dataService.getAxiosData("http://localhost:8000/writers", this.megjelenit);
-
+        $(window).on("torles", (event) => {
+            console.log(event.detail);
+            console.log("szia")
+            console.log(event.detail.id)
+            this.dataService.deleteAxiosData("http://localhost:8000/api/writers", event.detail.id)
+        
+        });
+        /* this.dataService.putAxiosData("http://localhost:8000/api/writers",{
+            id: 3,
+            nev: "Vörös Dominik",
+            szul: 2002,
+        }) */
+        
     }
-    megjelenit(list){
+    megjelenites(list){
         console.log(list)
+        const szuloElem = $(".tarolo");
+        const megjelenito = new Megjelenit(list, szuloElem);
+        
+    }
+    hibakezeles(uzenet){
+        console.log(uzenet)
     }
 }
-
 export default Controller
--- frontend/mvc/model/DataService.js
+++ frontend/mvc/model/DataService.js
@@ -3,15 +3,15 @@ class DataService{
 
     }
 
-    getAxiosData(url, callback,){
+    getAxiosData(url, callback, hibacallback){
         axios.get(url)
         .then(function (response) {
-          console.log("Válasz objektum",response);
+         /*  console.log("Válasz objektum",response); */
           console.log("adatok",response.data);
           console.log("státusz",response.status);
           console.log("Státusz szöveg",response.statusText);
-          console.log("Válasz fejléc",response.headers);
-          console.log("Válasz config",response.config);
+/*           console.log("Válasz fejléc",response.headers);
+          console.log("Válasz config",response.config); */
           callback(response.data)
           
         })
@@ -32,6 +32,28 @@ class DataService{
             console.log("hiba",error);
           })
         
+    }
+    deleteAxiosData(url, id){
+      axios
+      .delete(`${url}/${id}`)
+      .then((response)=> {
+        console.log("RESP", response);
+    })
+      .catch((error)=> {
+        console.log("hiba",error);
+      })
+    }
+    putAxiosData(url, data){
+      console.log(data)
+      console.log(`${url}/${data.id}`);
+      axios
+      .put(`${url}/${data.id}`,data)
+      .then((response) =>{
+        console.log("RESP", response);
+      })
+      .catch((error)=>{
+        console.log("hiba",error);
+      })
     }
 }
 
